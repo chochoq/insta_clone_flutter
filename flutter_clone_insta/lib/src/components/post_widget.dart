dd6f612d43ff6f48949db78faf8fd3c6ce86b21c
chochoq <aubepe@gmail.com>
2022-03-07 23:16:01 +0900

    (style) 스타일 padding 추가

--- flutter_clone_insta/lib/src/components/post_widget.dart
+++ flutter_clone_insta/lib/src/components/post_widget.dart
@@ -8,27 +8,30 @@ class PostWidget extends StatelessWidget {
   const PostWidget({Key? key}) : super(key: key);
 
   Widget _header() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        AvatarWidget(
-          type: AvatarType.TYPE3,
-          thumbPath:
-              'https://i.ytimg.com/vi/5rWYzT4VlLU/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB_RKMSsJ2NqkKntw7nR0Ktt52wdw',
-          size: 40,
-          nickname: '스윗스ㅅㅇㅅ스위스',
-        ),
-        GestureDetector(
-          onTap: () {},
-          child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: ImageData(
-              IconsPath.postMoreIcon,
-              width: 30,
-            ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          AvatarWidget(
+            type: AvatarType.TYPE3,
+            thumbPath:
+                'https://i.ytimg.com/vi/5rWYzT4VlLU/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB_RKMSsJ2NqkKntw7nR0Ktt52wdw',
+            size: 40,
+            nickname: '스윗스ㅅㅇㅅ스위스',
           ),
-        )
-      ],
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: ImageData(
+                IconsPath.postMoreIcon,
+                width: 30,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -39,80 +42,92 @@ class PostWidget extends StatelessWidget {
   }
 
   Widget _infoCount() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            ImageData(
-              IconsPath.likeOffIcon,
-              width: 65,
-            ),
-            const SizedBox(width: 15),
-            ImageData(
-              IconsPath.replyIcon,
-              width: 60,
-            ),
-            const SizedBox(width: 15),
-            ImageData(
-              IconsPath.directMessage,
-              width: 55,
-            ),
-          ],
-        ),
-        ImageData(
-          IconsPath.bookMarkOffIcon,
-          width: 50,
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              ImageData(
+                IconsPath.likeOffIcon,
+                width: 65,
+              ),
+              const SizedBox(width: 15),
+              ImageData(
+                IconsPath.replyIcon,
+                width: 60,
+              ),
+              const SizedBox(width: 15),
+              ImageData(
+                IconsPath.directMessage,
+                width: 55,
+              ),
+            ],
+          ),
+          ImageData(
+            IconsPath.bookMarkOffIcon,
+            width: 50,
+          ),
+        ],
+      ),
     );
   }
 
   Widget _infoDesc() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        const Text(
-          '좋아요 150',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Text(
+            '좋아요 150',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        ExpandableText(
-          '내용용죽겟지\n내용ㅇㅇㅇㅇㅇ\n헤헤헤\n dsdasda',
-          prefixText: '닉네임',
-          prefixStyle: TextStyle(fontWeight: FontWeight.bold),
-          expandText: '더보기',
-          collapseText: '접기',
-          maxLines: 3,
-          expandOnTextTap: true,
-          collapseOnTextTap: true,
-          linkColor: Colors.grey,
-          onPrefixTap: () {
-            print('닉네임 클릭');
-          },
-        ),
-      ],
+          ExpandableText(
+            '내용용죽겟지\n내용ㅇㅇㅇㅇㅇ\n헤헤헤\n dsdasda',
+            prefixText: '닉네임',
+            prefixStyle: TextStyle(fontWeight: FontWeight.bold),
+            expandText: '더보기',
+            collapseText: '접기',
+            maxLines: 3,
+            expandOnTextTap: true,
+            collapseOnTextTap: true,
+            linkColor: Colors.grey,
+            onPrefixTap: () {
+              print('닉네임 클릭');
+            },
+          ),
+        ],
+      ),
     );
   }
 
   Widget _replyTextBtn() {
-    return GestureDetector(
-      onTap: () {},
-      child: const Text('댓글 46개 모두보기',
-          style: TextStyle(
-            fontSize: 13,
-            color: Colors.grey,
-          )),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: GestureDetector(
+        onTap: () {},
+        child: const Text('댓글 46개 모두보기',
+            style: TextStyle(
+              fontSize: 13,
+              color: Colors.grey,
+            )),
+      ),
     );
   }
 
   Widget _dateAgo() {
-    return const Text(
-      '1일전',
-      style: TextStyle(
-        color: Colors.grey,
-        fontSize: 11,
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Text(
+        '1일전',
+        style: TextStyle(
+          color: Colors.grey,
+          fontSize: 11,
+        ),
       ),
     );
   }
@@ -120,7 +135,7 @@ class PostWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.all(20),
+        margin: const EdgeInsets.symmetric(vertical: 15),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
